be0c6a94400d85b9a68acde05f5f4409844be77c
thomtrep <thomtrepanier@gmail.com>
2024-02-01 14:22:31 -0500

    Changed new feature

--- src/main/java/pl/touk/sputnik/Main.java
+++ src/main/java/pl/touk/sputnik/Main.java
@@ -63,6 +63,6 @@ public final class Main {
 
     private static void printWelcomeMessage() {
         System.out.println("Sputnik version " + Main.class.getPackage().getImplementationVersion());
-        System.out.println("Test Print");
+        System.out.println("Test Print #2");
     }
 }
