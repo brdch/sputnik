797de07cf6260bca4ceb7cb96cadf6b3399b3aaa
thomtrep <thomtrepanier@gmail.com>
2024-02-01 14:59:26 -0500

    Added review formatter test

--- src/test/java/pl/touk/sputnik/review/ReviewFormatterTest.java
+++ src/test/java/pl/touk/sputnik/review/ReviewFormatterTest.java
@@ -1,15 +1,49 @@
 package pl.touk.sputnik.review;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import pl.touk.sputnik.configuration.Configuration;
 import pl.touk.sputnik.configuration.GeneralOption;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ReviewFormatterTest {
 
+    @Mock
+    private ReviewFile file1;
+
+    @Mock
+    private ReviewFile file2;
+
+    @Test
+    void shouldStringifyToEmptyStringWhenObjectIsNull() {
+        Configuration configMock = mock(Configuration.class);
+        when(configMock.getProperty(GeneralOption.MESSAGE_COMMENT_FORMAT)).thenReturn("{0}{1}{2}");
+        when(configMock.getProperty(GeneralOption.MESSAGE_PROBLEM_FORMAT)).thenReturn("{0}{1}");
+
+        ReviewFormatter formatter = new ReviewFormatter(configMock);
+        String result = formatter.stringify(null);
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void shouldStringifyObjectToStringRepresentation() {
+        Configuration configMock = mock(Configuration.class);
+        when(configMock.getProperty(GeneralOption.MESSAGE_COMMENT_FORMAT)).thenReturn("{0}{1}{2}");
+        when(configMock.getProperty(GeneralOption.MESSAGE_PROBLEM_FORMAT)).thenReturn("{0}{1}");
+
+        ReviewFormatter formatter = new ReviewFormatter(configMock);
+        Review review = new Review(asList(file1, file2), formatter);
+        String result = formatter.stringify(review);
+
+        assertThat(result).isEqualTo(review.toString());
+    }
+    
     @Test
     void shouldFormatProblemsAndComments() {
         Configuration configMock = mock(Configuration.class);
